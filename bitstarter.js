206874fac95e3bafd7c149990c293a81a4c69a37
natechavin <natechavin@gmail.com>
2013-07-18 04:14:54 +0000

    Edit bitstarter.js

--- bitstarter.js
+++ bitstarter.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readFileSync(filename);
+buf.toString('utf-8')
+'Hello world!\u0000?k\t'
 });
 
 var port = process.env.PORT || 5000;

34e85821ed2e39a4867eb576c1f7599dca81c8df
natechavin <natechavin@gmail.com>
2013-09-30 04:28:12 +0000

    Fixed bitstarter.js

--- bitstarter.js
+++ bitstarter.js
@@ -7,7 +7,7 @@ var HTMLImport = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
   response.writeHead(200, {
       'Content-type': 'text/html; charset=utf-8'
-app.use('/page',express.static(__dirname+'/page'));
+
   });
   response.end(HTMLImport);
 }); // <--- here close the function call
@@ -16,3 +16,4 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+app.use('/page',express.static(__dirname+'/page'));
